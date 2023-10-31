d5dc0be93f6332a5f419714cdd7f3ffa3b2f75b0
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-10-31 16:16:16 -0500

    add favicon + sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://robbywebb.com',
+  site: 'https://www.robbywebb.com',
+  integrations: [
+    sitemap({
+      lastmod: new Date(),
+    }),
+  ],
 })
