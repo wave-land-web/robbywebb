d9aa4a3bd0099a5e13f166abe089d5400013825e
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-11-07 16:06:09 -0600

    inline stylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.robbywebb.com',
+  build: {
+    inlineStylesheets: 'always',
+  },
   integrations: [
     sitemap({
       lastmod: new Date(),
