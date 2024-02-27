206e15a56d6c72663bc9d5674f80b7690346d890
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-02-27 10:15:10 -0600

    navigation + homepage styling, active nav links, ViewTransitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.robbywebb.com',
+  scopedStyleStrategy: 'class',
   build: {
     inlineStylesheets: 'always',
   },
