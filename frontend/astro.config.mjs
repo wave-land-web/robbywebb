1e643a9fd608ab55922b1ee75e5da33c69fcaff2
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-03-20 18:18:44 -0500

    add Sanity studio

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config'
-import icon from 'astro-icon'
 import sitemap from '@astrojs/sitemap'
+import sanity from '@sanity/astro'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +14,12 @@ export default defineConfig({
     icon(),
     sitemap({
       lastmod: new Date(),
+    }),
+    sanity({
+      projectId: 'o6m55439',
+      dataset: 'production',
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
     }),
   ],
 })
