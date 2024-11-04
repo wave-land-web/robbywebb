1488b56be108d851aa2356ea40dcc3c7e8b83457
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-11-04 13:23:00 -0600

    chore(app): update studio deps

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'o6m55439',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
